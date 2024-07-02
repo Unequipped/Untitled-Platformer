250b657b7bb4476384d5f8fc8310da6d06a17818
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-07-02 23:37:50 +0100

    fixed movement input

--- Scenes/Entities/input_manager.gd
+++ Scenes/Entities/input_manager.gd
@@ -2,12 +2,7 @@ class_name InputManager
 extends Node
 
 static func x_inp():
-	var x_dir:int = 0
-	if Input.is_action_pressed("left"):
-		x_dir = -1
-	elif Input.is_action_pressed("right"):
-		x_dir = 1
-	return x_dir
+	return Input.get_axis("left", "right")
 
 static func y_inp():
 	var y_dir:int = 0
