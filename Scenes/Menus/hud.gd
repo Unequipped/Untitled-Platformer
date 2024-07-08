7fedc923eb942c14329d4e311ce12bef1fc602ed
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-07-08 22:31:11 +0100

    removed player 2 from hud

--- Scenes/Menus/hud.gd
+++ Scenes/Menus/hud.gd
@@ -7,8 +7,6 @@ extends CanvasLayer
 
 @export var p1_hp: ProgressBar
 @export var p1_en: ProgressBar
-@export var p2_hp: ProgressBar
-@export var p2_en: ProgressBar
 
 func _ready():
 	visible = true
@@ -20,10 +18,6 @@ func _process(delta):
 		p1_hp.max_value = player_one.hp_comp.MAX_HEALTH
 		p1_hp.value = player_one.hp_comp.health
 		p1_en.value = player_one.roll_cd
-	if player_two:
-		p2_hp.max_value = player_two.hp_comp.MAX_HEALTH
-		p2_hp.value = player_two.hp_comp.health
-		p2_en.value = player_two.roll_cd
 
-	if p1_hp.value <= 0 or p2_hp.value <= 0:
+	if p1_hp.value <= 0:
 		pass
