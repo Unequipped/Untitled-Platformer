f26816f30170b255ef0b5f3e4f24bc74a8265355
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-07-09 22:19:12 +0100

    added assets & input display

--- Scenes/Menus/hud.gd
+++ Scenes/Menus/hud.gd
@@ -7,16 +7,34 @@ extends CanvasLayer
 @export var p1_hp: ProgressBar
 @export var p1_en: ProgressBar
 
+@export var w_key: BaseButton
+@export var a_key: BaseButton
+@export var s_key: BaseButton
+@export var d_key: BaseButton
+
 func _ready():
 	visible = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player_one:
 		p1_hp.max_value = player_one.hp_comp.MAX_HEALTH
 		p1_hp.value = player_one.hp_comp.health
 		p1_en.value = player_one.roll_cd
 
-	if p1_hp.value <= 0:
-		pass
+	key_press_display()
+
+
+func key_press_display(): # there is probably a better way of doing this but it works for now
+	w_key.button_pressed = false
+	a_key.button_pressed = false
+	s_key.button_pressed = false
+	d_key.button_pressed = false
+	if Input.is_action_pressed("up"):
+		w_key.button_pressed = true
+	if Input.is_action_pressed("left"):
+		a_key.button_pressed = true
+	if Input.is_action_pressed("down"):
+		s_key.button_pressed = true
+	if Input.is_action_pressed("right"):
+		d_key.button_pressed = true
