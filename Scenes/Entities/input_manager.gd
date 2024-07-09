5cfe14a3a532a1e0120b1ecba7a915929e0cb160
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-07-09 17:49:04 +0100

    input manager clean up

--- Scenes/Entities/input_manager.gd
+++ Scenes/Entities/input_manager.gd
@@ -2,19 +2,17 @@ class_name InputManager
 extends Node
 
 #need to rename vars
-@export var jump_buffer_timer: float = 0.5
+@export var jump_buffer_timer: float = 0.7
 var jump_buffer_time: float = 0.0
 
 func _input(event):
 	if Input.is_action_just_pressed("up"):
 		jump_buffer_time = jump_buffer_timer
 
+
 func _physics_process(delta):
-	if jump_buffer_time >= 0:
-		jump_buffer_time -= 0.1
-	else:
-		jump_buffer_time = 0
-	print(jump_buffer_time)
+	inc_jump_buffer()
+
 
 static func x_inp():
 	return Input.get_axis("left", "right")
@@ -37,3 +35,10 @@ func get_direction():
 	direction.x = x_inp()
 	direction.y = y_inp()
 	return direction
+
+
+func inc_jump_buffer():
+	if jump_buffer_time >= 0:
+		jump_buffer_time -= 0.1
+	else:
+		jump_buffer_time = 0
