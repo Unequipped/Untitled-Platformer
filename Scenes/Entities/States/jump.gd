1831ed11100e0f932d449c14876a3b4fa5730b93
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-10-05 19:13:42 +0100

    FINALLY GOT VARIABLE JUMP WORKING (I took out the old lines in input script)

--- Scenes/Entities/States/jump.gd
+++ Scenes/Entities/States/jump.gd
@@ -1,11 +1,14 @@
 extends State
 
 @export var actor: CharacterBody2D
-
 @export var jump_force: int = 110
+
+var min_jump_duration:int = 10 # minimum duration for the jump
+var jump_duration:int = 0
 
 func enter():
 	jump_apply()
+	jump_duration = 0
 
 func exit():
 	pass
@@ -15,6 +18,9 @@ func update(delta):
 
 func physics_update(delta):
 	switch_state()
+	if jump_duration < min_jump_duration:
+		print(jump_duration)
+		jump_duration += 1
 
 func switch_state():
 	if actor.is_on_floor() and not actor.velocity.y < 0:
@@ -23,7 +29,7 @@ func switch_state():
 	if actor.velocity.y > 0:
 		Transitioned.emit(self, "fall")
 	
-	elif inputManager.jump_inp_released():
+	elif inputManager.jump_inp_released() and jump_duration >= min_jump_duration:
 		Transitioned.emit(self, "fall")
 
 func jump_apply():
