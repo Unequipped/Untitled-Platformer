d674192d7c6a7ab8748e7af5d01d21ff3d1923f0
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-10-07 17:02:32 +0100

    attack animations and bugs more or less all fixed (attack2 is back)

--- Scenes/Entities/States/attack.gd
+++ Scenes/Entities/States/attack.gd
@@ -15,19 +15,14 @@ func exit():
 
 func update(delta):
 	attack_playing = actor.animate() # returns true if animation is playing
-	print(attack_playing)
 
 func physics_update(delta):
 	actor.velocity.x = 0
 	switch_state()
 
 func switch_state():
-	#if inputManager.attack_inp() and !attack_playing and actor.is_on_floor():
-			#Transitioned.emit(self, "attack2")
-	
-	#elif !attack_playing: #There seems to be this really weird attack bug and I think it's related to this
-		#Transitioned.emit(self, "idle")
-
 	if !attack_playing:
+		if inputManager.attack_inp():
+			Transitioned.emit(self, "attack2")
 		if actor.is_on_floor():
 			Transitioned.emit(self, "idle")
--- Scenes/Entities/States/attack2.gd
+++ Scenes/Entities/States/attack2.gd
@@ -7,16 +7,13 @@ extends State
 var attack_playing:bool
 
 func enter():
-	attack_playing = true
+	actor.animate(str(name))
 
 func exit():
 	actor.apply_dmg()
 
 func update(delta):
-	if actor.animator.frame < end_frame:
-		attack_playing = actor.animate(str(name)) # THIS NEEDS TO BE UPDATED SO ITS SAFER TO USE
-	else:
-		attack_playing = false
+	attack_playing = actor.animate()
 
 func physics_update(delta):
 	actor.velocity.x = 0
@@ -24,11 +21,7 @@ func physics_update(delta):
 
 func switch_state():
 	if !attack_playing:
-		Transitioned.emit(self, "idle")
-
-	if !inputManager.attack_inp() and !attack_playing:
 		if actor.is_on_floor():
 			Transitioned.emit(self, "idle")
-		
 		else:
 			Transitioned.emit(self, "fall")
