6f2fa2b1f8824c23f72e18a6ec6b7c9e933408ab
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-10-16 19:12:17 +0100

    gave 'sight' to skeleton and now follows player

--- Scenes/Entities/enemies/AI.gd
+++ Scenes/Entities/enemies/AI.gd
@@ -1,4 +1,12 @@
 class_name AI extends LogicManager
 
+@export var sight_raycast_R: RayCast2D
+@export var sight_raycast_L: RayCast2D
+
 func get_direction():
-	return Vector2.RIGHT
+	if sight_raycast_L.get_collider() != null:
+		return Vector2.LEFT
+	elif sight_raycast_R.get_collider() != null:
+		return Vector2.RIGHT
+	else:
+		return Vector2.ZERO
