f54b7366f87a03d8e0704de47014557302a8bf7d
Unequipped <77971199+Unequipped@users.noreply.github.com>
2024-10-17 21:57:12 +0100

    fixed flipping enemy sprite

--- Scenes/Entities/enemies/AI.gd
+++ Scenes/Entities/enemies/AI.gd
@@ -3,10 +3,15 @@ class_name AI extends LogicManager
 @export var sight_raycast_R: RayCast2D
 @export var sight_raycast_L: RayCast2D
 
-func get_direction():
+func get_sight():
 	if sight_raycast_L.get_collider() != null:
-		return Vector2.LEFT
+		return -1
 	elif sight_raycast_R.get_collider() != null:
-		return Vector2.RIGHT
+		return 1
 	else:
-		return Vector2.ZERO
+		return 0
+
+func get_direction():
+	var direction:Vector2 = Vector2.ZERO
+	direction.x = get_sight()
+	return direction
