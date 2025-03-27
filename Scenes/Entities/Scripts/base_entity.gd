4ef2bb1dc99d4d046cc544470941d61920a4dae6
Unequipped <77971199+Unequipped@users.noreply.github.com>
2025-03-27 15:42:33 +0000

    restructuring files

--- Scenes/Entities/Scripts/base_entity.gd
+++ Scenes/Entities/Scripts/base_entity.gd
@@ -2,18 +2,6 @@ class_name Entity extends CharacterBody2D
 
 @export var animator: AnimatedSprite2D
 
-var is_hit: bool # A variable for checking if the entity has been hit
-
-func pivot(facing):
-	return
-
-func rec_hit(damage): 
-	is_hit = true
-	print("ok")
-	# register damage taken to hp
-	# end
-	pass
-
 func animate(animation: String = "", anim_to_check: String = "", reverse:bool = false): ## Animates & returns if animation is playing
 	if animation:
 		if !(animator.animation == anim_to_check and animator.is_playing() and anim_to_check != ""):
