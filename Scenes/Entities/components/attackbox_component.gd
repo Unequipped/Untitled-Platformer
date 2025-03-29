d8ec2c3152f8ddf08059df5215cd092dcfbdc5f4
Unequipped <77971199+Unequipped@users.noreply.github.com>
2025-03-29 00:01:55 +0000

    Deleted 'components' folder. Deleted 'shared' folder

--- Scenes/Entities/components/attackbox_component.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func look_for_target(target: String, value):
-	#print(get_overlapping_bodies())
-	for body in get_overlapping_bodies():
-		if body.name == target:
-			body.apply_damage(value)
-		else:
-			pass
