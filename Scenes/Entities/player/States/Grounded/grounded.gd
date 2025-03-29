6db93aae0b99db223ee3e5220eac05f2b61ab6de
Unequipped <77971199+Unequipped@users.noreply.github.com>
2025-03-29 12:09:28 +0000

    clean up

---
+++ Scenes/Entities/player/States/Grounded/grounded.gd
@@ -0,0 +1,16 @@
+class_name Grounded extends PlayerState
+
+func enter():
+	pass
+
+func exit():
+	pass
+
+func update(_delta) -> void:
+	pass
+
+func physics_update(_delta) -> void:
+	pass
+
+func switch_cond():
+	pass
