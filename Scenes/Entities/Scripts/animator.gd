6ece827b30adbba06ec6551811a3494e6fce53d0
Unequipped <77971199+Unequipped@users.noreply.github.com>
2025-05-10 21:39:13 +0100

    added sprite + animations back

---
+++ Scenes/Entities/Scripts/animator.gd
@@ -0,0 +1,19 @@
+class_name Animator extends Node
+
+@export var animated_sprite: AnimatedSprite2D
+@export var state_machine: StateMachine
+
+var state_to_animate: State
+
+func play_animation(anim_name: String, speed: float = 1):
+	animated_sprite.play(anim_name, speed)
+
+func get_current_state():
+	return state_machine.current_state
+
+func _process(delta):
+	if state_to_animate:
+		play_animation(state_to_animate.name)
+
+func _physics_process(delta):
+	state_to_animate = get_current_state()
