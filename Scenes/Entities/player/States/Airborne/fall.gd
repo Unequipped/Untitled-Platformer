006be3cf1e196278e8788300d23a112bf1435fba
Unequipped <77971199+Unequipped@users.noreply.github.com>
2025-05-10 16:40:03 +0100

    Added: Coyote Jump + tuning

--- Scenes/Entities/player/States/Airborne/fall.gd
+++ Scenes/Entities/player/States/Airborne/fall.gd
@@ -4,7 +4,7 @@ func enter():
 	pass
 
 func exit():
-	pass
+	actor.velocity.y = 0 # this is needed to allow coyote jump to be strong enough
 
 func update(_delta) -> void:
 	pass
@@ -15,6 +15,9 @@ func physics_update(_delta) -> void:
 func switch_cond():
 	if actor.is_on_floor() and input_manager.x_inp() != 0:
 		state_machine.change_state(&"Run")
+	
+	if !actor.is_on_floor() and input_manager.jump_inp() and input_manager.coyote_time > 0:
+		state_machine.change_state(&"Jump")
 	
 	if actor.is_on_wall() and actor.get_last_slide_collision():
 		if actor.get_last_slide_collision().get_normal().x == -input_manager.x_inp():
