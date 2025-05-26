e230c8b8c12bea107f1ef7847638aeb89f4aea2d
Unequipped <77971199+Unequipped@users.noreply.github.com>
2025-05-26 10:53:06 +0100

    attempting to angle player on slope

--- Scenes/Entities/Scripts/animator.gd
+++ Scenes/Entities/Scripts/animator.gd
@@ -1,9 +1,13 @@
 class_name Animator extends Node
 
+@export var actor: CharacterBody2D
 @export var animated_sprite: AnimatedSprite2D
+@export var sprite_pivot: Node2D
 @export var state_machine: StateMachine
 
 var state_to_animate: State
+var floor_angle: float
+var node_angle: float
 
 func play_animation(anim_name: String, speed: float = 1):
 	if anim_name == "WallKick":
@@ -22,6 +26,17 @@ func get_current_state():
 func _process(delta):
 	if state_to_animate:
 		play_animation(state_to_animate.name)
+	angle_actor()
+
+
+func angle_actor():
+	pass
+	#floor_angle = actor.get_floor_angle()
+	#node_angle = sprite_pivot.rotation
+	#print(floor_angle, node_angle)
+	#if actor.is_on_floor() and floor_angle != -node_angle:
+		#sprite_pivot.rotation = floor_angle
+
 
 func _physics_process(delta):
 	state_to_animate = get_current_state()
