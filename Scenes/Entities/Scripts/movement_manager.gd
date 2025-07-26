e44a714a01a1b80b2503dced7c0bfa83747bea28
Unequipped <77971199+Unequipped@users.noreply.github.com>
2025-07-26 18:05:46 +0100

    going back to basic movement code

--- Scenes/Entities/Scripts/movement_manager.gd
+++ Scenes/Entities/Scripts/movement_manager.gd
@@ -14,12 +14,11 @@ var SPEED: int = 0
 @export var y_max: int = 200
 
 ## x movement lerping
-@export var x_accel:float = 0.7
-@export var x_decel:float = 0.4 #0.4 is better
+@export var x_accel:float = 10
+@export var x_decel:float = 10 #0.4 is better
 
-@export var x_air_decel:float = 0.06
-@export var x_air_accel:float = 0.2
-
+@export var x_air_accel:float = 10
+@export var x_air_decel:float = 10
 
 func _ready():
 	SPEED = MAX_SPEED
@@ -30,7 +29,7 @@ func x_movement(target_speed, rate):
 		if actor.is_on_floor():
 			apply_x_accel(x_dir, target_speed, rate)
 		else:
-			apply_x_accel(x_dir, target_speed, x_air_accel)
+			apply_x_accel(x_dir, target_speed, rate)
 	else:
 		if actor.is_on_floor():
 			apply_x_decel(x_decel)
@@ -38,16 +37,10 @@ func x_movement(target_speed, rate):
 			apply_x_decel(x_air_decel)
 
 func apply_x_accel(x_dir, target_speed, rate):
-	#actor.velocity.x = move_toward(actor.velocity.x, target_speed * x_dir, rate)
-	#if input_provider.run_inp() and input_provider.y_inp() >= 0:
-		#actor.velocity.x = lerpf(actor.velocity.x, MAX_SPEED * x_dir, rate)
-	#else:
-		actor.velocity.x = lerpf(actor.velocity.x, target_speed * x_dir, rate)
+	actor.velocity.x = move_toward(actor.velocity.x, target_speed * x_dir, rate)
 
 func apply_x_decel(rate):
-	actor.velocity.x = lerpf(actor.velocity.x, 0.0, rate)
-	if abs(actor.velocity.x) <= 0.001:
-		actor.velocity.x = 0
+	actor.velocity.x = move_toward(actor.velocity.x, 0.0, rate)
 
 func apply_gravity():
 	actor.velocity.y = move_toward(actor.velocity.y, y_max, y_accel)
